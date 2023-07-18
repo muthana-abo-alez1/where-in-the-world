f680a431f82ca86608fd6def5200763cb25bb40e
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2023-07-18 13:56:08 +0300

    add code Store the state of the dark mode in the local storage

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,28 +1,25 @@
 var flagDarkMode = false;
 
-window.addEventListener("load",checkDarkMode)
+window.addEventListener("load", checkDarkMode);
 
-function checkDarkMode(){
-    var items = localStorage.getItem("dark")
-    if(items=="true"){
-    document.getElementById("dark-toggle")
-    let element=document.body
-    element.classList.toggle("dark-mode")
-    var checkDark = document.getElementById("dark-toggle")
+function checkDarkMode() {
+  var items = localStorage.getItem("dark");
+  if (items == "true") {
+    document.getElementById("dark-toggle");
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+    var checkDark = document.getElementById("dark-toggle");
     checkDark.checked = true;
-
+  }
 }
+function dark_mode() {
+  var checkDark = document.getElementById("dark-toggle");
+  document.getElementById("dark-toggle");
+  let element = document.body;
+  if (!checkDark.checked) {
+    element.classList.toggle("dark-mode");
+  } else {
+    element.classList.remove("dark-mode");
+  }
+  localStorage.setItem("dark", !checkDark.checked);
 }
-function dark_mode(){
-    var checkDark = document.getElementById("dark-toggle")
-    document.getElementById("dark-toggle")
-    let element=document.body
-    console.log(checkDark.checked)
-    if(!checkDark.checked){
-    element.classList.toggle("dark-mode")
-    }else{
-        element.classList.remove("dark-mode");
-    }
-    localStorage.setItem("dark", !checkDark.checked);
-}
-
